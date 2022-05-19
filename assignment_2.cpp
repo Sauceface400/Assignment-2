227293df67a4d6e54796b5a40b238c8df51d2e9f
Sauceface400 <59503496+Sauceface400@users.noreply.github.com>
2022-05-19 08:10:42 +0800

    Update assignment_2.cpp

--- assignment_2.cpp
+++ assignment_2.cpp
@@ -12,10 +12,12 @@ void details(string subcriberName, int phoneNumber, string international){
 }
 
 int main(){
-    float total[3];
+    float total;
+    float overall[3] = {total, total, total};
+    int sum = 0;
 
     // the for loops is use to loop through the 10 subcribers
-    for (int i = 0; i < sizeof(total)/sizeof(total[0]); i++){
+    for (int i = 0; i < 3; i++){
         string x;
         int y;
         string z;
@@ -31,63 +33,64 @@ int main(){
 
         float distance;
         int duration;
+
         cout << "Enter distnace: " << endl;
         cin >> distance;
         cout << "Enter duration: " << endl;
         cin >> duration;
 
+        // this is to add the total price of the subcribers
+        sum += overall[i] + total;
+
         // this is for the people who eneter yes
         if(z == "Y"){
             if (distance < 25){
                 price = 0.55;
-                float total = price * duration;
+                total = price * duration;
                 cout << "Your total price is RM" << total << endl;
             }else if (distance >= 25 and distance < 75){
                 price = 0.85;
-                float total = price * duration;
+                total = price * duration;
                 cout << "Your total price is RM" << total << endl;
             }else if (distance >= 75 and distance < 300){
                 price = 1.20;
-                float total = price * duration;
+                total = price * duration;
                 cout << "Your total price is RM" << total << endl;
             }else if (distance >= 300 and distance <= 1000){
                 price = 2.20;
-                float total = price * duration;
+                total = price * duration;
                 cout << "Your total price is RM" << total << endl;
             }else{
                 price = 3.20;
-                float total = price * duration;
+                total = price * duration;
                 cout << "Your total price is RM" << total << endl;
             } 
         // this is for the people who enter No 
         }else if (z == "N"){
             if (distance < 25){
                 price = 0.35;
-                float total = price * duration;
+                total = price * duration;
                 cout << "Your total price is RM" << total << endl;
             }else if (distance >= 25 and distance < 75){
                 price = 0.65;
-                float total = price * duration;
+                total = price * duration;
                 cout << "Your total price is RM" << total << endl;
             }else if (distance >= 75 and distance < 300){
                 price = 1.00;
-                float total = price * duration;
+                total = price * duration;
                 cout << "Your total price is RM" << total << endl;
             }else if (distance >= 300 and distance <= 1000){
                 price = 2.00;
-                float total = price * duration;
+                total = price * duration;
                 cout << "Your total price is RM" << total << endl;
             }else{
                 price = 3.00;
-                float total = price * duration;
+                total = price * duration;
                 cout << "Your total price is RM" << total << endl;
             } 
         }
-        // total[3] = {price};
-        // this is to add the total price of the subcribers
-        price + price + price;
         
+        // this is to output the total price of the 10 subs
+        cout << "Total is RM" << sum << endl;
     }
-    // this is to output the total price of the 10 subs
-    cout << "Total is RM" << price << endl;
 }
